5ccb6cfa9cb1bc1f9b409cae62986641775eafb3
Jeff Sylvester <sylvesterjj@gmail.com>
2024-12-10 20:19:15 -0500

    initial check in'

---
+++ resource.ts
@@ -0,0 +1,116 @@
+﻿import {RestClient} from "./rest-client";
+
+export type ResourceType<T extends Resource> = new(_restClient: RestClient) => T;
+
+export class LinkParameter {
+    name: string
+    type: string
+    defaultValue: string
+    listOfValues: string[]
+}
+
+export class Link {
+    constructor(link: any, resource: Resource) {
+        this.resource = resource;
+        this.href = link.href;
+        this.verb = link.verb;
+
+        if (link.fields) {
+            for (let fieldName in link.fields) {
+                let field = link.fields[fieldName] as LinkParameter;
+                field.name = fieldName;
+                this.parameters.push(field);
+            }
+        }
+
+        if (link.parameters) {
+            for (let parameterName in link.parameters) {
+                let parameter = link.parameters[parameterName] as LinkParameter;
+                parameter.name = parameterName;
+                this.parameters.push(parameter);
+            }
+        }
+    }
+    href: string;
+    verb: string;
+    parameters: LinkParameter[] = [];
+    resource: Resource;
+}
+
+export class Resource {
+    private readonly _links: Map<string, Link> = new Map<string, Link>();
+    private readonly _originalValues: Map<string, any> = new Map<string, any>();
+    constructor(private readonly _restClient: RestClient) {
+    }
+
+    response: Response;
+    get statusCode(): number {
+        if (!this.response) {
+            return 0;
+        }
+        return this.response.status
+    }
+
+    get isOk(): boolean {
+        if (!this.response) {
+            return false;
+        }
+        return this.response.ok;
+    }
+
+    private getLink(linkName: string) : Link {
+        return this._links.get(linkName)
+    }
+
+    protected async executeLink<T extends Resource>(r: new(...args : any) => T, linkName: string, values: any = {}) : Promise<T> {
+        const link = this.getLink(linkName)
+        if (link == null) {
+            return;
+        }
+
+        return await this._restClient.executeLink(r, link, values)
+    };
+
+    protected hasLink(linkName: string): boolean {
+        return this.getLink(linkName) != null;
+    }
+
+    protected getResourceList<T extends Resource>(resourceType: ResourceType<T>, propertyName: string): T[] {
+        const list: Array<T> = []
+
+        propertyName = propertyName.toLowerCase();
+        if (!this._originalValues.has(propertyName)) {
+            return list;
+        }
+
+        for (let source of this._originalValues.get(propertyName)) {
+            const resource = new resourceType(this._restClient)
+            resource.populateData(source)
+            list.push(resource);
+        }
+
+        return list;
+    }
+
+    populateData(source : any) : void {
+        for (const sourceProperty in source) {
+            const value = source[sourceProperty]
+            this._originalValues.set(sourceProperty.toLowerCase(), value);
+
+            if (sourceProperty === "_links") {
+                for (let linkName in value) {
+                    this._links.set(linkName, new Link(value[linkName], this));
+                }
+                continue;
+            }
+
+            for (let destinationProperty in this) {
+                if (sourceProperty.toLowerCase() !== destinationProperty.toLowerCase()) {
+                    continue;
+                }
+
+                this[destinationProperty] = value;
+            }
+        }
+    }
+}
---
+++ rest-client.ts
@@ -0,0 +1,153 @@
+﻿import {Link, Resource, ResourceType} from "./resource";
+
+export class RestClientError extends Error {
+    constructor(response: Response) {
+        super(response.statusText);
+        this.statusCode = response.status;
+        this.response = response;
+    }
+    statusCode: number
+    response: Response
+}
+
+export class RestClient {
+    private readonly _baseUrl: string;
+    private _setCookie: string;
+
+    constructor(_baseUrl: string) {
+        this._baseUrl = _baseUrl;
+    }
+
+    //normally this should not be used: the browser handles it, but it needs to be done manually when being used outside a browser (ie: PlayWright)
+    handleCookies: boolean;
+    //be default, non success status codes will throw an error. If this is set to false, an empty class can be returned instead which will contain the response
+    throwExceptions: boolean = true;
+
+    async get<T extends Resource> (resourceType: ResourceType<T>, path: string = ""): Promise<T> {
+        return await this.execute(resourceType, 'GET', path )
+    }
+
+    async executeLink<T extends Resource> (resourceType: ResourceType<T>, link: Link, values: any = {}): Promise<T> {
+        const verb = link.verb ? link.verb : "GET";
+        const params = {};
+        let setParameterFromSource = function (parameter: string, source: any): boolean {
+            for (let value in source) {
+                if (parameter.toLowerCase() !== value.toLowerCase()) {
+                    continue;
+                }
+
+                params[parameter] = source[value];
+                return true;
+            }
+            return false;
+        }
+
+        //loop through the expected parameters and populate them, first looking in passed in values, second looking in the resource, finally looking at the default value
+        for (const parameter of link.parameters) {
+            if (setParameterFromSource(parameter.name, values)) {
+                continue;
+            }
+
+            if (setParameterFromSource(parameter.name, link.resource)) {
+                continue;
+            }
+
+            if (parameter.defaultValue) {
+                params[parameter.name] = values[parameter.defaultValue];
+            }
+        }
+
+        return await this.execute(resourceType, verb, link.href, params);
+    }
+
+    async execute<T extends Resource> (resourceType: ResourceType<T>, verb: string, path: string, params: any = {}): Promise<T> {
+        const url = new URL(this._baseUrl);
+        if (path != "") {
+            if (path.includes("?")) {
+                const parts = path.split("?")
+                url.pathname = parts[0]
+                url.search = parts[1]
+            } else {
+                url.pathname = path;
+            }
+        }
+
+        const contentType =  verb == "PATCH"
+            ? "application/merge-patch+json"
+            : "application/json";
+
+        if (Object.keys(params).length > 0 && (verb == "GET" || verb == "DELETE")) {
+            for (const param in params ) {
+                url.searchParams.set(param, params[param]);
+            }
+        }
+
+        const hasBody = (Object.keys(params).length > 0 && (verb == "POST" || verb == "PUT" || verb == "PATCH"));
+
+        let headers = {
+            'Content-Type': contentType,
+            'Accept': 'application/slysoft+json, application/json'
+        }
+
+        if (this._setCookie) {
+            headers['Cookie'] = this._setCookie;
+        }
+
+        let response: Response
+        if (hasBody) {
+            response = await fetch(url.toString(), {
+                credentials: 'include',
+                method: verb,
+                headers: headers,
+                body: JSON.stringify(params)
+            })
+        } else {
+            response = await fetch(url.toString(), {
+                credentials: 'include',
+                method: verb,
+                headers: headers
+            })
+        }
+
+        const responseToReturn = response.clone();
+        if (!response.ok) {
+            if (this.throwExceptions) {
+                throw new RestClientError(responseToReturn);
+            } else {
+                const resource = new resourceType(this);
+                resource.response = responseToReturn;
+                return resource;
+            }
+        }
+
+        if (this.handleCookies) {
+            const setCookie = response.headers.get('set-cookie');
+            if (setCookie) {
+                this._setCookie = setCookie;
+            }
+        }
+
+        let json: any = {};
+        if (this.responseIsJson(response)) {
+            try {
+                json = await response.json();
+            } catch {
+                //eat the exception and just use an empty object- it should be json, but there was an issue parsing it
+            }
+        }
+
+        const resource = new resourceType(this);
+        resource.populateData(json)
+        resource.response = responseToReturn;
+
+        return resource;
+    }
+
+    private responseIsJson(response: Response): boolean {
+        const receivedContentType =response.headers.get('content-type');
+        if (!receivedContentType) {
+            return false;
+        }
+        return (receivedContentType.includes('application/json') || receivedContentType.includes('application/slysoft+json'));
+    }
+}
