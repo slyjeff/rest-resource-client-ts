ca0ca0b57c8c8ea5e8d1ba5c1b46479e97550463
Jeff Sylvester <sylvesterjj@gmail.com>
2025-04-02 08:59:56 -0400

    removes cookie setter

--- src/rest-client.ts
+++ src/rest-client.ts
@@ -17,8 +17,6 @@ export class RestClient {
     private readonly _baseUrl: string;
     private _setCookie: string | undefined;
 
-    //normally this should not be used: the browser will handle cookies, but in some situations, the user will need to inject logic to store the cookie
-    public cookieSetter: (cookie: string) => void;
     //normally this should not be used: the browser handles it, but it needs to be done manually when being used outside a browser (ie: PlayWright)
     public handleCookies: boolean = false;
     //be default, non success status codes will throw an error. If this is set to false, an empty class can be returned instead which will contain the response
@@ -26,7 +24,6 @@ export class RestClient {
 
     constructor(_baseUrl: string) {
         this._baseUrl = _baseUrl;
-        this.cookieSetter = (cookie: string) => this._setCookie = cookie;
     }
 
     async get<T extends Resource> (resourceType: ResourceType<T>, path: string = ""): Promise<T> {
@@ -129,7 +126,7 @@ export class RestClient {
         if (this.handleCookies) {
             const cookie = response.headers.get('set-cookie');
             if (cookie) {
-                this.cookieSetter(cookie)
+                this._setCookie = cookie;
             }
         }
 
